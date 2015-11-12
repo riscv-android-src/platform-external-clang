2b8df1678b238b3313076b11431964ec6a9a5c14
Dan Albert <danalbert@google.com>
2015-11-12 15:26:07 -0800

    Add a script that builds a full host toolchain.
    Bug: http://b/17441393
    Change-Id: I2f3d2aa7c6182cfb4946f314a509c6ca4a1779ed

---
+++ build.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import argparse
+import datetime
+import multiprocessing
+import os
+import subprocess
+
+
+THIS_DIR = os.path.realpath(os.path.dirname(__file__))
+ORIG_ENV = dict(os.environ)
+
+
+def android_path(path=''):
+    return os.path.realpath(os.path.join(THIS_DIR, '../..', path))
+
+
+def build_path(subdir):
+    # OUT_DIR will be set when built on the build server. In that case we need
+    # to make sure our build directories are in OUT_DIR rather than
+    # ANDROID_BUILD_TOP.
+    if 'OUT_DIR' in ORIG_ENV:
+        return os.path.join(ORIG_ENV['OUT_DIR'], subdir)
+    else:
+        return android_path(subdir)
+
+
+def build(out_dir):
+    products = (
+        'aosp_arm',
+        'aosp_arm64',
+        'aosp_mips',
+        'aosp_mips64',
+        'aosp_x86',
+        'aosp_x86_64',
+    )
+    for product in products:
+        build_product(out_dir, product)
+
+
+def build_product(out_dir, product):
+    env = dict(ORIG_ENV)
+    env['OUT_DIR'] = out_dir
+    env['DISABLE_LLVM_DEVICE_BUILDS'] = 'true'
+    env['DISABLE_RELOCATION_PACKER'] = 'true'
+    env['FORCE_BUILD_LLVM_COMPONENTS'] = 'true'
+    env['FORCE_BUILD_SANITIZER_SHARED_OBJECTS'] = 'true'
+    env['SKIP_LLVM_TESTS'] = 'true'
+    env['TARGET_PRODUCT'] = product
+
+    jobs_arg = '-j{}'.format(multiprocessing.cpu_count())
+    targets = ['clang-toolchain']
+    subprocess.check_call(
+        ['make', jobs_arg] + targets, cwd=android_path(), env=env)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--build-name', default=datetime.date.today().strftime('%Y%m%d'),
+        help='Release name for the package.')
+
+    multi_stage_group = parser.add_mutually_exclusive_group()
+    multi_stage_group.add_argument(
+        '--multi-stage', action='store_true',
+        help='Perform multi-stage build (disabled by default).')
+    multi_stage_group.add_argument(
+        '--no-multi-stage', action='store_false', dest='multi_stage',
+        help='Do not perform multi-stage build.')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    stage_1_out_dir = build_path('stage1')
+    build(out_dir=stage_1_out_dir)
+    if args.multi_stage:
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    main()
