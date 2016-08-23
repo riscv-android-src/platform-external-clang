050bb709897ae6a7c1ea43645b95423db2cff1db
Pirama Arumuga Nainar <pirama@google.com>
2016-08-23 15:54:40 -0700

    Update patch level and version string
    Bug: http://b/30421084
    Change-Id: I2422f019badb3030fb82a6eb9909e72813a75ba9
    Test: Build with external/clang/build.py

--- include/clang/Config/config.h
+++ include/clang/Config/config.h
@@ -35,7 +35,7 @@
 /* Define if we have libxml2 */
 /* #undef CLANG_HAVE_LIBXML */
 
-#define PACKAGE_STRING "LLVM 3.8.271374"
+#define PACKAGE_STRING "LLVM 3.8.275480"
 
 /* The LLVM product name and version */
 #define BACKEND_PACKAGE_STRING PACKAGE_STRING
